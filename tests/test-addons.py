b307055acbd216cba20f0eefced5b6b3c9a09f6d
mowoe <git@mowoe.com>
2023-07-02 16:16:07 +0200

    adjust tests

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -5,9 +5,13 @@ from utils import microk8s_enable, wait_for_pod_state, microk8s_disable
 
 
 class TestAddons(object):
-    def test_python_demo_nginx(self):
-        microk8s_enable("python-hello-k8s")
-        wait_for_pod_state("", "default", "running", label="app=python-demo-nginx")
-        status = yaml.safe_load(sh.microk8s.status(format="yaml").stdout)
-        expected = {"python-hello-k8s": "enabled"}
-        microk8s_disable("python-hello-k8s")
+    def test_metallb_addon(self):
+        addon = "metallb"
+        ip_ranges = (
+            "192.168.0.105-192.168.0.105,192.168.0.110-192.168.0.111,192.168.1.240/28"
+        )
+        print("Enabling metallb")
+        microk8s_enable("{}:{}".format(addon, ip_ranges), timeout_insec=500)
+        validate_metallb_config(ip_ranges)
+        print("Disabling metallb")
+        microk8s_disable("metallb")
